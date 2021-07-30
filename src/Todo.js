6fe48e11c9ceba6385b0853e5e29e6f3585192c0
hfs <hfssiddiqui@gmail.com>
2021-07-30 14:43:44 -0700

    input component for entering/adding todo items

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,32 @@
 import {React, useState} from 'react';
 
 export default function Todo() {
+    const [inputText, setInputText] = useState('');
+const [items, setItems] = useState([]);
 
-    
+const addItem = (text) => {
+    let newItem = {
+        id: `${+new Date()}`,
+        done: false,
+        text
+    }
+
+    setItems([...items, newItem]);
+    setInputText('');
+}
+
+const handleKeyPress = (e) => {
+if (e.key === 'Enter') {
+    addItem(inputText);
+}
+}
+
+
     return (
         <div>
-            Todo
+            <h1>todos</h1>
+            <input placeholder='What needs to be done' value={inputText} onChange={e => setInputText(e.target.value)} onKeyPress={handleKeyPress} />
+            
         </div>
     );
 }
